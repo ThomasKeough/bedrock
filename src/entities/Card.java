a74c497defb0c16b203b7f528f323737e903031b
Dharma <dharma.ong@mail.utoronto.ca>
2023-10-30 16:10:52 -0400

    added entities

--- src/entities/Card.java
+++ src/entities/Card.java
@@ -1,10 +1,19 @@
 package entities;
 
+import java.util.Dictionary;
+import java.util.HashMap;
+
 public interface Card {
     String getName();
 
     String getId();
 
     Integer getHP();
+
+    HashMap<String, Integer> getAttacks();
+
+    String getCardArt();
+
+
 
 }
--- src/entities/Player.java
+++ src/entities/Player.java
@@ -1,10 +1,16 @@
 package entities;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public interface Player {
     String getName();
 
-    String getCollection();
+    Deck getCurrentDeck();
 
+    // returns a list of all the cards in the player's collection
+    ArrayList<Card> getCollection();
 
+    // returns a mapping of decks that the user has
+    HashMap<String, Deck> getDecks();
 
 }
