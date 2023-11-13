966c5a8116f45bc12c0d0df2b4185f0cdd99538c
pangstan <stanley.pang@mail.utoronto.ca>
2023-11-13 16:46:47 -0500

    Added instance variables and methods to BuildDeckViewModel

--- src/interface_adapters/add_to_collection/AddToCollectionPresenter.java
+++ src/interface_adapters/add_to_collection/AddToCollectionPresenter.java
@@ -1,4 +1,16 @@
 package interface_adapters.add_to_collection;
 
-public class AddToCollectionPresenter {
+import use_cases.add_card_to_collection.AddToCollectionOutputBoundary;
+import use_cases.add_card_to_collection.AddToCollectionOutputData;
+
+public class AddToCollectionPresenter implements AddToCollectionOutputBoundary {
+    @Override
+    public void prepareSuccessView(AddToCollectionOutputData outputData) {
+
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+
+    }
 }
