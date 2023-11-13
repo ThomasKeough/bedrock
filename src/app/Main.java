e86f6f405e82b2fc9201bac0a362ef5715b932dc
pangstan <stanley.pang@mail.utoronto.ca>
2023-11-13 16:58:33 -0500

    Basic additions to the main method

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,14 @@
 package app;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
+    public static void main(String[] args) {
+        JFrame application = new JFrame("Pokemon Menu");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        CardLayout cardLayout = new CardLayout();
+
+    }
 }
