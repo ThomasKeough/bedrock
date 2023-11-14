f47026590a41ecdec6e04fa3b28bd5e1741c7735
Stanley <stanleypang666@gmail.com>
2023-11-14 15:43:31 -0500

    Created ViewManagerModel in interface_adapters and ViewManager in view, worked on Main a little

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,14 +1,22 @@
 package app;
 
+import interface_adapters.ViewManagerModel;
+import view.ViewManager;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame application = new JFrame("Pokemon Menu");
+        JFrame application = new JFrame("Pokemon");
         application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         CardLayout cardLayout = new CardLayout();
 
+        JPanel views = new JPanel(cardLayout);
+        application.add(views);
+
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        new ViewManager(views, cardLayout, viewManagerModel);
     }
 }
