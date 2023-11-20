2aa89d609d8b378ef74affe6cda32692e969e258
Dharma <dharma.ong@mail.utoronto.ca>
2023-11-20 15:17:29 -0500

    added start of a game loop implementation and swap implementation

---
+++ src/use_cases/swap/SwapOutputData.java
@@ -0,0 +1,11 @@
+package use_cases.swap;
+
+public class SwapOutputData {
+    private final boolean result;
+
+    public SwapOutputData(boolean result) {
+        this.result = result;
+    }
+
+    public boolean getResult() { return this.result; }
+}
