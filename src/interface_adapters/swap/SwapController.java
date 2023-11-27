fcef3dc89122268e785d087c9d56f3f43b0f9797
Thomas <thomas.keough@mail.utoronto.ca>
2023-11-27 15:56:57 -0500

    built bones of swap presenter

---
+++ src/interface_adapters/swap/SwapController.java
@@ -0,0 +1,4 @@
+package interface_adapters.swap;
+
+public class SwapController {
+}
---
+++ src/interface_adapters/swap/SwapPresenter.java
@@ -0,0 +1,17 @@
+package interface_adapters.swap;
+
+import use_cases.swap.SwapOutputBoundary;
+import use_cases.swap.SwapOutputData;
+
+public class SwapPresenter implements SwapOutputBoundary {
+    @Override
+    public void prepareSucessView(SwapOutputData swapOutputData) {
+        //TODO
+    }
+
+    @Override
+    public void prepareFailView(SwapOutputData swapOutputData) {
+        //TODO
+
+    }
+}
