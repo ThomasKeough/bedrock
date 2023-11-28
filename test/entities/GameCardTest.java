09785a7806a373ca78180ef63320675da94f05c6
Thomas <thomas.keough@mail.utoronto.ca>
2023-11-28 12:28:06 -0500

    built test for GameCard.getName(), bug in CommonCardFactory weaknesses and resistances

--- test/entities/GameCardTest.java
+++ test/entities/GameCardTest.java
@@ -1,4 +1,55 @@
-import static org.junit.Assert.*;
-public class GameCardTest {
-  
+package entities;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameCardTest {
+    Card card;
+    GamePokemon gameCard;
+
+    @BeforeEach
+    void init() {
+        CardFactory cardFactory = new CommonCardFactory();
+        card = cardFactory.create("sv3pt5-143","Snorlax");
+        gameCard = new GameCard(card);
+    }
+
+    @Test
+    void getName() {
+        assertEquals("Snorlax", gameCard.getName());
+    }
+
+    @Test
+    void getType() {
+    }
+
+    @Test
+    void getHP() {
+    }
+
+    @Test
+    void getAttack() {
+    }
+
+    @Test
+    void hasFainted() {
+    }
+
+    @Test
+    void isOnField() {
+    }
+
+    @Test
+    void faint() {
+    }
+
+    @Test
+    void takeDamage() {
+    }
+
+    @Test
+    void swap() {
+    }
 }
