b52b133bf322d9511d348366601f949254c3073a
Thomas <thomas.keough@mail.utoronto.ca>
2023-12-01 13:22:27 -0500

    m implementing swap viewmodel, swap state, and swap presenter.

--- src/interface_adapters/swap/SwapPresenter.java
+++ src/interface_adapters/swap/SwapPresenter.java
@@ -1,12 +1,27 @@
 package interface_adapters.swap;
 
+import interface_adapters.ViewModel;
 import use_cases.swap.SwapOutputBoundary;
 import use_cases.swap.SwapOutputData;
 
 public class SwapPresenter implements SwapOutputBoundary {
+
+    private final SwapViewModel swapViewModel;
+
+    private final ViewModel viewModel;
+    public SwapPresenter(SwapViewModel swapViewModel,
+                         ViewModel viewModel) {
+        this.swapViewModel = swapViewModel;
+        this.viewModel = viewModel;
+    }
+
     @Override
     public void prepareSucessView(SwapOutputData swapOutputData) {
-        //TODO
+        SwapState swapState = swapViewModel.getState();
+        swapState.setSwaps(swapOutputData);
+        swapViewModel.setState(swapState);
+        swapViewModel.firePropertyChanged();
+
     }
 
     @Override
