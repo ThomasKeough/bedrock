d1b1f8f97eff9a2d01359a9373f2f5bc4aee43ca
Thomas <thomas.keough@mail.utoronto.ca>
2023-12-01 12:46:44 -0500

    atLimit() was checking if the number of cards is <= limit, should be >=

--- src/entities/CommonCollection.java
+++ src/entities/CommonCollection.java
@@ -25,6 +25,6 @@ public class CommonCollection implements Collection {
     }
 
     public Boolean atLimit() {
-        return cards.size() <= limit;
+        return cards.size() >= limit;
     }
 }
