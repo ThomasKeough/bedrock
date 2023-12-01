2a051c000c9ba972a09abc52d6ebc070b7449363
Thomas <thomas.keough@mail.utoronto.ca>
2023-12-01 15:34:42 -0500

    started tests

--- test/use_cases/game/GameInteractorTest.java
+++ test/use_cases/game/GameInteractorTest.java
@@ -1,4 +1,58 @@
+package use_cases.game;
+
+import data.TradingCardGameDAO;
+import interface_adapters.attack.AttackPresenter;
+import interface_adapters.game.GamePresenter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import use_cases.attack.AttackInputBoundary;
+import use_cases.attack.AttackInteractor;
+
+import java.io.File;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class GameInteractorTest {
-  
+    private GameInteractor gameInteractor;
+
+    @BeforeEach
+    void init() {
+        GameDataAccessInterface gameDataAccessInterface = new TradingCardGameDAO(new File("circulating_pokemon_cards.csv"));
+        GameOutputBoundary gamePresenter = new GamePresenter();
+        AttackInputBoundary attackInteractor = new AttackInteractor(new AttackPresenter());
+        gameInteractor = new GameInteractor(gameDataAccessInterface, gamePresenter, attackInteractor);
+
+    }
+
+    @Test
+    void onGameStateUpdate() {
+    }
+
+    @Test
+    void resetAttackButton() {
+    }
+
+    @Test
+    void handleAttack() {
+    }
+
+    @Test
+    void execute() {
+    }
+
+    @Test
+    void determineWinner() {
+    }
+
+    @Test
+    void coinFlip() {
+    }
+
+    @Test
+    void initializeCards() {
+    }
+
+    @Test
+    void runGameLoop() {
+    }
 }
