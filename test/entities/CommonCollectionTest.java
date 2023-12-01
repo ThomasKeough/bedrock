56fc8ffe6ee0c25cb140c5ce4e63c3298f7c5181
Thomas <thomas.keough@mail.utoronto.ca>
2023-12-01 12:07:17 -0500

    created CommonCollection test suite

---
+++ test/entities/CommonCollectionTest.java
@@ -0,0 +1,20 @@
+package entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommonCollectionTest {
+
+    @Test
+    void getCards() {
+    }
+
+    @Test
+    void getLimit() {
+    }
+
+    @Test
+    void atLimit() {
+    }
+}
