291656c2b0fa450891e687fe76d3490751a0ba0d
Thomas <thomas.keough@mail.utoronto.ca>
2023-12-03 17:09:57 -0500

    made deck test file

---
+++ test/entities/CommonDeckTest.java
@@ -0,0 +1,32 @@
+package entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommonDeckTest {
+
+    @Test
+    void getDeckName() {
+    }
+
+    @Test
+    void setDeckName() {
+    }
+
+    @Test
+    void getCards() {
+    }
+
+    @Test
+    void numPokemonAlive() {
+    }
+
+    @Test
+    void getActivePokemon() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
